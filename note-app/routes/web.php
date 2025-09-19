1b89d6a7421ad4002b1a9b0477b1c2dee58a662b
Костя Исупов <isupov_kostya@bk.ru>
2025-09-20 04:17:23 +0600

    feat and refactor: add Note Controller add create, edit, index, show note view for notes upgrade routes

--- note-app/routes/web.php
+++ note-app/routes/web.php
@@ -3,6 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\GoodbyeController;
+use App\Http\Controllers\NoteController;
 
 Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
 Route::get('/goodbye', [GoodbyeController::class, 'goodbye'])->name('goodbye');
+
+Route::get('/note', [NoteController::class, 'index'])->name('note.index');
+Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
+Route::post('/note', [NoteController::class, 'store'])->name('note.store');
+Route::get('/note/{id}', [NoteController::class, 'show'])->name('note.show');
+Route::get('/note/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
+Route::put('/note/{id}', [NoteController::class, 'update'])->name('note.update');
+Route::delete('/note/{id}', [NoteController::class, 'destroy'])->name('note.destroy');
+
