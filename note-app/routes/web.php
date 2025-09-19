55796f952514713c4c2882ee621068d6411b9c04
Костя Исупов <isupov_kostya@bk.ru>
2025-09-20 04:23:29 +0600

    feat ans refactor: add views and upgrade routes add create, index, edit, show view for Todo upgrade routes: add Todo routes

--- note-app/routes/web.php
+++ note-app/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\GoodbyeController;
 use App\Http\Controllers\NoteController;
+use App\Http\Controllers\TodoController;
 
 Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
 Route::get('/goodbye', [GoodbyeController::class, 'goodbye'])->name('goodbye');
@@ -16,3 +17,10 @@ Route::get('/note/{id}/edit', [NoteController::class, 'edit'])->name('note.edit'
 Route::put('/note/{id}', [NoteController::class, 'update'])->name('note.update');
 Route::delete('/note/{id}', [NoteController::class, 'destroy'])->name('note.destroy');
 
+Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
+Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
+Route::post('/todo', [TodoController::class, 'store'])->name('todo.store');
+Route::get('/todo/{id}', [TodoController::class, 'show'])->name('todo.show');
+Route::get('/todo/{id}/edit', [TodoController::class, 'edit'])->name('todo.edit');
+Route::put('/todo/{id}', [TodoController::class, 'update'])->name('todo.update');
+Route::delete('/todo/{id}', [TodoController::class, 'destroy'])->name('todo.destroy');
